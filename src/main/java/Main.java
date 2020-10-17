cc4cd587254c27b34dfb3136fb50ce058371042d
Aleksandr Prokopyev <aleksandr.prokopyev@jetbrains.com>
2020-10-17 14:55:44 +0300

    Fix cos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,22 @@
+import base.CosCalculator;
+import base.LogCalculator;
 import base.SinCalculator;
+import base.TestFunction;
 
 public class Main {
 
     public static void main(String[] args) {
-        SinCalculator calc = new SinCalculator();
-        System.out.println(calc.fun(0));
-        System.out.println(calc.fun(Math.PI/2));
-        System.out.println(calc.fun(Math.PI/4));
-        System.out.println(calc.fun(Math.PI/6));
-        System.out.println(calc.fun(Math.PI/3));
+        printFuncs(SinCalculator.getInstance());
+        printFuncs(CosCalculator.getInstance());
+//        printFuncs(new LogCalculator(5));
+    }
+
+    private static void printFuncs(TestFunction function) {
+        System.out.println(function.fun(0));
+        System.out.println(function.fun(Math.PI/2));
+        System.out.println(function.fun(Math.PI/4));
+        System.out.println(function.fun(Math.PI/6));
+        System.out.println(function.fun(Math.PI/3));
     }
 
 }
--- src/main/java/base/CosCalculator.java
+++ src/main/java/base/CosCalculator.java
@@ -2,9 +2,15 @@ package base;
 
 public class CosCalculator implements TestFunction {
 
+    private static final CosCalculator INSTANCE = new CosCalculator();
+
     @Override
     public double fun(double arg) {
         final double sin = SinCalculator.getInstance().fun(arg);
         return Math.sqrt(1d - sin * sin);
+    }
+
+    public static CosCalculator getInstance() {
+        return INSTANCE;
     }
 }
